b1bdfcf4b1291c84125605803cf0e631416e39da
Shankhadeep Maiti <maitishankhadeep123@gmail.com>
2024-08-02 20:19:28 +0530

    Update knn-class.R

--- knn-class.R
+++ knn-class.R
@@ -1,5 +1,12 @@
 suppressPackageStartupMessages(library(tidyverse))
 
-data <- read_csv('R/k-nearest-neighbors/breast-cancer-wisconsin.csv', show_col_types = FALSE)
+data <- read_csv('R/k-nearest-neighbors/breast-cancer-wisconsin.csv', show_col_types = FALSE) # nolint
 data <- subset(data, select = -c(class))
 head(data, 5)
+
+library(caTools)
+set.seed(255)
+
+split = sample.split(data$cancer, SplitRatio = 0.75)
+train = subset(data, split == TRUE)
+test = subset(data, split == FALSE)
