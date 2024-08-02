8a55c02afc74d5e73713e2cb5a1a56dda671ffd7
Shankhadeep Maiti <maitishankhadeep123@gmail.com>
2024-08-02 23:14:27 +0530

    Update knn-class.R

--- knn-class.R
+++ knn-class.R
@@ -1,12 +1,32 @@
 suppressPackageStartupMessages(library(tidyverse))
 
 data <- read_csv('R/k-nearest-neighbors/breast-cancer-wisconsin.csv', show_col_types = FALSE) # nolint
-data <- subset(data, select = -c(class))
+data <- subset(data, select = -c(id))
 head(data, 5)
+
+data <- as.data.frame(lapply(data, function(x) {
+  if(is.factor(x) || is.character(x)) {
+    as.numeric(as.character(x))
+  } else {
+    as.numeric(x)
+  }
+}))
+
 
 library(caTools)
 set.seed(255)
 
-split = sample.split(data$cancer, SplitRatio = 0.75)
-train = subset(data, split == TRUE)
-test = subset(data, split == FALSE)
+split <- sample.split(data$class, SplitRatio = 0.75)
+train <- subset(data, split == TRUE)
+test <- subset(data, split == FALSE)
+
+scaled_train <- scale(train[-10])
+scaled_test <- scale(test[-10])
+
+library(class)
+test <- knn(
+  train = scaled_train,
+  test = scaled_test,
+  cl = train$class,
+  k=10
+)
