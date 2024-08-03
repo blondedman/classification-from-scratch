42d40ce2469a1f8f138616bde1f5821688924f32
Shankhadeep Maiti <maitishankhadeep123@gmail.com>
2024-08-03 09:58:42 +0530

    Update knn-class.R

--- knn-class.R
+++ knn-class.R
@@ -1,8 +1,8 @@
 suppressPackageStartupMessages(library(tidyverse))
 
 data <- read_csv('R/k-nearest-neighbors/breast-cancer-wisconsin.csv', show_col_types = FALSE) # nolint
+
 data <- subset(data, select = -c(id))
-head(data, 5)
 
 data <- as.data.frame(lapply(data, function(x) {
   if(is.factor(x) || is.character(x)) {
@@ -12,21 +12,32 @@ data <- as.data.frame(lapply(data, function(x) {
   }
 }))
 
+data[is.na(data)] <- -99999
+
+head(data, 5)
+
 
 library(caTools)
 set.seed(255)
 
-split <- sample.split(data$class, SplitRatio = 0.75)
+split <- sample.split(data$class, SplitRatio = 0.2)
 train <- subset(data, split == TRUE)
 test <- subset(data, split == FALSE)
 
-scaled_train <- scale(train[-10])
-scaled_test <- scale(test[-10])
+scaled_train <- scale(train[,1:10])
+scaled_test <- scale(test[,1:10])
 
 library(class)
-test <- knn(
+predict <- knn(
   train = scaled_train,
   test = scaled_test,
   cl = train$class,
-  k=10
+  k=1
 )
+
+actual <- test$class
+cm <- table(actual,predict)
+cm
+
+accuracy <- sum(diag(cm))/length(actual)*100 
+accuracy
