8da33c00881fada9da0f192a0d04facd8e2278af
Shankhadeep Maiti <maitishankhadeep123@gmail.com>
2024-08-04 20:13:26 +0530

    Update knn-caret.R

--- knn-caret.R
+++ knn-caret.R
@@ -1,3 +1,21 @@
+suppressPackageStartupMessages(library(tidyverse))
+
+data <- read_csv('R/k-nearest-neighbors/breast-cancer-wisconsin.csv', show_col_types = FALSE) # nolint
+
+data <- subset(data, select = -c(id))
+
+data <- as.data.frame(lapply(data, function(x) {
+  if(is.factor(x) || is.character(x)) {
+    as.numeric(as.character(x))
+  } else {
+    as.numeric(x)
+  }
+}))
+
+data[is.na(data)] <- -99999
+
+head(data, 5)
+
 suppressPackageStartupMessages(library(caret))
 set.seed(255)
 
@@ -8,14 +26,20 @@ index <- createDataPartition(data$class, times = 1, p = 0.8, list = FALSE)
 train <- data[index,]
 test <- data[-index,]
 
-preValues <- preProcess(train, method = c("center", "scale"))
-trainTransformed <- predict(preValues, train)
-testTransformed <- predict(preValues, test)
+preprocessing <- preProcess(train, method = c("center", "scale"))
+trainTransformed <- predict(preprocessing, train)
+testTransformed <- predict(preprocessing, test)
 
 knnModel <- train(
-  not_fully_paid ~ ., 
+  class ~ ., 
   data = trainTransformed, 
   method = "knn", 
   trControl = trainControl(method = "cv"), 
   tuneGrid = data.frame(k = c(3,5,7))
+)
+
+best <- knn(
+  class ~ .,
+  data = trainTransformed,
+  k = knnModel$bestTune$k
 )
